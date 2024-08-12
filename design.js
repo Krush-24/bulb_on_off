bed4021b5fc8869ef02f957ba14e4b182e9ea821
Krush-24 <138600135+Krush-24@users.noreply.github.com>
2024-08-12 12:24:47 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,10 @@
+function bulbON()
+{
+    // console.log("Bulb ON function");
+    document.getElementById("image").src = "./bulb-on.png";
+}
+function bulbOFF()
+{
+    // console.log("Bulb OFF function");
+    document.getElementById("image").src = "./bulb-off.png";
+}
